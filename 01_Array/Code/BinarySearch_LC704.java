577a27de49588cc1bcadc163655c6c68339915e3
Shaw <yinshaw@shu.edu.com>
2024-06-14 19:30:32 +0800

    add BinarySearch as first Test.

--- 01_Array/Code/BinarySearch_LC704.java
+++ 01_Array/Code/BinarySearch_LC704.java
@@ -1,14 +1,32 @@
 /**
 * Run Successful Total Time Field
-* Your Time:
-* 1:  
+Website: https://leetcode.cn/problems/binary-search/
+* Your Time | Error Description
+* 1:  18:37-19:12  35min  right下表忘记减-1;  防溢出:l+(r-l)/2; l<=r
+* 2:  
+* 3:  
+* 4:  
 */
-
 public class BinarySearch_LC704 {
-    
+    public static int search(int[] nums, int target) {
+        int l=0,r=nums.length-1,mid=0;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            System.out.println(l+""+r+""+mid);
+            if(nums[mid]==target){
+                return mid;
+            }else if(target<nums[mid]){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        System.out.println("hello");
-
+        int arr[] = new int[]{-1,0,2,3,5,9,12};
+        int target = 4;
+        System.out.println(search(arr,target));
     }
     
 }
